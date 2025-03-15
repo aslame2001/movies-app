14120fedb9572340679407877737dbf2aee830b2
Aslam E <156638896+aslame2001@users.noreply.github.com>
2025-03-15 16:57:43 +0530

    Update index.blade.php

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -159,23 +159,31 @@
     $(document).ready(function() {
         $('.favorite-form').on('submit', function(e) {
             e.preventDefault();
-
             var form = $(this);
             var movieId = form.find('.favorite-btn').data('movie-id');
+            var button = form.find('.favorite-btn');
+            var heartIcon = button.find('svg');
 
             $.ajax({
-                url: '{{ route("favorites.add") }}',
+                url: '{{ route("favorites.add") }}', 
                 method: 'POST',
                 data: form.serialize(),
                 success: function(response) {
                     if (response.success) {
-                        alert('Movie added to favorites!');
-                        // Optionally change the heart icon to filled (e.g., bi-heart-fill)
-                        form.find('.bi-heart').replaceWith(
-                            '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">' +
-                            '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>' +
-                            '</svg>'
-                        );
+                        alert(response.message);
+                        if (response.message === 'Movie added to favorites!') {
+                            heartIcon.replaceWith(
+                                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">' +
+                                '<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>' +
+                                '</svg>'
+                            );
+                        } else if (response.message === 'Movie removed from favorites!') {
+                            heartIcon.replaceWith(
+                                '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-heart" viewBox="0 0 16 16">' +
+                                '<path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>' +
+                                '</svg>'
+                            );
+                        }
                     }
                 },
                 error: function(xhr) {
